4dc95a0fe044bcd20e3e0e13f3599925e1f4f3cb
tranqhuy46 <tranquochuy4698@gmail.com>
2022-11-08 22:58:55 +0700

    chore: setup base design tokens

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,21 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <p>The joke bible</p>
+    <div className="App">
+      <div className="cj-joke-card">
+        <div className="cj-joke-card__header">
+          <div className="cj-joke-card__title">Lawyer joke</div>
+        </div>
+        <div className="cj-joke-card__body">
+          <div className="cj-joke-card__copy">
+            A Lawyer dies and goes to Heaven. "There must be some mistakes", the
+            lawyer argues. A Lawyer dies and goes to Heaven. "There must be some
+            mistakes", the lawyer argues. A Lawyer dies and goes to Heaven.
+            "There must be some mistakes", the lawyer argues. A Lawyer dies and
+            goes to Heaven. "There must be some mistakes", the lawyer argues.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
