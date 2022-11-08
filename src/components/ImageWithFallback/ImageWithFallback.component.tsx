d2a16408f5c7708573c4cd440cdd24451cf57bbc
tranqhuy46 <tranquochuy4698@gmail.com>
2022-11-09 00:23:29 +0700

    feat: update ImageWithFallback styles, more robust

--- src/components/ImageWithFallback/ImageWithFallback.component.tsx
+++ src/components/ImageWithFallback/ImageWithFallback.component.tsx
@@ -2,13 +2,14 @@ import type {ImageWithFallbackProps} from './ImageWithFallback.type';
 
 import './ImageWithFallback.scss';
 
+import clsx from 'clsx';
 import React from 'react';
 
 const ImageWithFallback: React.FC<ImageWithFallbackProps> = (props) => {
-  const {src, fallback, type = 'image/webp', ...delegated} = props;
+  const {src, fallback, type = 'image/webp', className, ...delegated} = props;
 
   return (
-    <picture className="cj-image-with-fallback">
+    <picture className={clsx('cj-image-with-fallback', className)}>
       <source
         className="cj-image-with-fallback__source"
         srcSet={src}
