d508456dea3cdb383974f9da5f4ff29fc22058f6
tranqhuy46 <tranquochuy4698@gmail.com>
2022-11-09 00:05:25 +0700

    feat: update ImageWithFallback styles

--- src/components/ImageWithFallback/ImageWithFallback.component.tsx
+++ src/components/ImageWithFallback/ImageWithFallback.component.tsx
@@ -1,14 +1,24 @@
 import type {ImageWithFallbackProps} from './ImageWithFallback.type';
 
+import './ImageWithFallback.scss';
+
 import React from 'react';
 
 const ImageWithFallback: React.FC<ImageWithFallbackProps> = (props) => {
   const {src, fallback, type = 'image/webp', ...delegated} = props;
 
   return (
-    <picture>
-      <source srcSet={src} type={type} />
-      <img src={fallback} {...delegated} />
+    <picture className="cj-image-with-fallback">
+      <source
+        className="cj-image-with-fallback__source"
+        srcSet={src}
+        type={type}
+      />
+      <img
+        className="cj-image-with-fallback__img"
+        src={fallback}
+        {...delegated}
+      />
     </picture>
   );
 };
