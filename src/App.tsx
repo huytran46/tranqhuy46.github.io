26e0a5173a3232a29498548daf4e752164bcd73a
tranqhuy46 <tranquochuy4698@gmail.com>
2022-11-09 18:58:02 +0700

    opt: clean code

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,61 @@ import './App.scss';
 
 import React from 'react';
 
-import JokeCard from './components/JokeCard';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <JokeCard
-        title="Lawyer joke"
-        description='A Lawyer dies and goes to Heaven. "There must be some mistakes", the
-          lawyer argues. A Lawyer dies and goes to Heaven. "There must be some
-          mistakes", the lawyer argues. A Lawyer dies and goes to Heaven. "There
-          must be some mistakes", the lawyer argues. A Lawyer dies and goes to
-          Heaven. "There must be some mistakes", the lawyer argues.'
-      />
+    <div className="cj-app">
+      <Navbar />
+
+      <div>
+        this is a jumbotron with the background
+        <div>
+          this is imposter
+          <div>
+            this is form group
+            <label htmlFor=""> this is a label</label>
+            <p> this is a desc</p>
+            <div>
+              this is a input group
+              <input type="text" placeholder="this is an input" />
+              <span>this is icon search</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <main>
+        this is a body
+        <div>
+          this is categories list
+          <span>this is category tag 1</span>
+          <span>this is category tag 2</span>
+          <button>this is view all button</button>
+        </div>
+        <hr /> divider here
+        <div>
+          <div>this is category tag</div>
+          <div>
+            this is a preview list
+            <span> this is joke card 1</span>
+            <span> this is joke card 2</span>
+          </div>
+          <button>this is view more button</button>
+        </div>
+      </main>
+
+      <footer>
+        this is footer with background img
+        <div>
+          this is imposter
+          <div>
+            this is call to action container
+            <h5>this is call to action label</h5>
+            <button>this is submit button</button>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
