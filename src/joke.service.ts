9028578b8f02e5bf9e0820854b99339a83519fc9
tranqhuy46 <tranquochuy4698@gmail.com>
2022-11-09 23:57:37 +0700

    feat: setup axios, joke services

---
+++ src/joke.service.ts
@@ -0,0 +1,45 @@
+import type {JokeModel} from './models/joke';
+
+import AxiosService from './lib/axios';
+
+interface AllJokesResponse<T> {
+  total: number;
+  result: T[];
+}
+
+/**
+ *
+ * @param query
+ * @returns
+ */
+export async function fetchAllJokes(
+  query = 'all',
+): Promise<AllJokesResponse<JokeModel>> {
+  try {
+    const response = await AxiosService.get<AllJokesResponse<JokeModel>>(
+      `/jokes/search?query=${query}`,
+    );
+    return (
+      response?.data ?? {
+        total: 0,
+        result: [],
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ *
+ * @param
+ * @returns {string[]} categories
+ */
+export async function fetchJokeCategories(): Promise<string[]> {
+  try {
+    const response = await AxiosService.get<string[]>('/jokes/categories');
+    return response?.data ?? [];
+  } catch (error) {
+    throw error;
+  }
+}
